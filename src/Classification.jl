fdc0c446037a520ca30ab2fbc448c44d141fffd9
Adarshkumar712 <Adarshkumar712.ak@gmail.com>
2020-03-31 12:38:49 +0530

    Added report_stats, debug Classification metrics

--- src/Classification.jl
+++ src/Classification.jl
@@ -6,12 +6,11 @@ using StatsBase: mean
 
 # Onehot_encode
 function onehot_encode(y, labels)
-    label = Dict(unique(y) .=> collect(labels))
-    onehot_arr = zeros(length(labels), length(y))
-    for i in 1:length(y)
-        onehot_arr[label[y[i]], i] = 1
-    end
-    onehot_arr
+   onehot_arr = zeros(length(labels), length(y))
+   for i in 1:length(y)
+       onehot_arr[Int(y[i]), i] = 1
+   end
+   onehot_arr
 end
 
 # Onecold
@@ -19,50 +18,51 @@ function onecold(y)
     onecold_arr = zeros(size(y,2))
     argmax_vec = argmax(y, dims = 1)
     for i in 1:size(y,2)
-        onecold_arr = argmax_vec[i].I[1]
+        onecold_arr[i] = argmax_vec[i].I[1]
     end
     return onecold_arr
-end 
+end
 
 # Confusion Matrix
-function Confusion_Matrix(y_pred, y_true)
+function confusion_matrix(y_pred, y_true)
     @assert size(y_pred) == size(y_true)
-    label_count = size(y_true, 2)
+    label_count = size(y_true, 1)
     ŷ = onehot_encode(onecold(y_pred), 1:label_count)
-    return transpose(y_true) * ŷ
+    return ŷ * transpose(y_true) 
 end
   
 # TFPN  
 function TFPN(y_pred, y_true)
     @assert size(y_pred) == size(y_true)
-    label_count = size(y_true, 2)
+    label_count = size(y_true, 1)
     TP = zeros(label_count) ; TN = zeros(label_count)
     FP = zeros(label_count) ; FN = zeros(label_count)
+    ConfusionMatrix = confusion_matrix(y_pred, y_true)
     for c in 1 : label_count
         TP[c] = ConfusionMatrix[c,c]
         FP[c] = sum(ConfusionMatrix[:,c]) - TP[c]
         FN[c] = sum(ConfusionMatrix[c,:]) - TP[c]
         TN[c] = sum(ConfusionMatrix) - TP[c] - FP[c] - FN[c]
     end
-    return TP, TN, FP, FN
+    return ConfusionMatrix,TP, TN, FP, FN
 end
 
 # Averaged Binary Accuracy 
-function Binary_accuracy(y_pred, y_true; threshold = 0.5)
+function binary_accuracy(y_pred, y_true; threshold = 0.5)
     @assert size(y_pred) == size(y_true)
-    return sum((y_pred .>= threshold) .== y_true) / size(y, 2)
+    return sum((y_pred .>= threshold) .== y_true) / size(y_true, 1)
 end
 
 # Average Categorical Accuracy
-function Categorical_accuracy(y_pred, y_true)
+function categorical_accuracy(y_pred, y_true)
     @assert size(y_pred) == size(y_true)
-    return sum(onecold(y_pred) .== onecold(y_true)) / size(y, 2)
+    return sum(onecold(y_pred) .== onecold(y_true)) / size(y_true, 2)
 end
 
 # Average Sparse Categorical Accuracy
-function Sparse_Categorical_Accuracy(y_pred, y_pred)
-    @assert size(y_pred) == size(y_true)
-    return sum(onecold(y_pred) .== y_true) / size(y, 2)
+function sparse_categorical(y_pred, y_true)
+    @assert size(y_pred, 2) == length(y_true)
+    return sum(onecold(y_pred) .== y_true) / size(y_true, 1)
 end
 
 # Top-k Categorical
@@ -71,7 +71,7 @@ function top_k_categorical(y_pred, y_true, k=3)
     count = 0
     sparse_y = onecold(y_true)
     for i in 1:size(y_true, 2)
-        top_k = partialsortperm(y_pred[:,i], 1:k)
+        top_k = partialsortperm(y_pred[:,i], 1:k, lt= >)
         for j in 1:k
             if top_k[j] == sparse_y[i]
                 count+=1
@@ -83,11 +83,10 @@ function top_k_categorical(y_pred, y_true, k=3)
 end
 
 # Top-k-sparse_categorical
-function top_k_sparse_categorical(y_pred, y_true)
-    @assert size(y_pred) == size(y_true)
+function top_k_sparse_categorical(y_pred, y_true; k=3)
     count = 0
-    for i in 1:size(y_true, 2)
-        top_k = partialsortperm(y_pred[:,i], 1:k)
+    for i in 1:length(y_true)
+        top_k = partialsortperm(y_pred[:,i], 1:k, lt= >)
         for j in 1:k
             if top_k[j] == y_true[i]
                 count+=1
@@ -100,22 +99,23 @@ end
 
 # Calculate Instances for each class in y
 function calc_instances(y)
-    instances = zeros(size(y, 1))
-    sparse_y = onecold(y)
-    for i in 1:length(weights)
-        instances[i] = sum(sparse_y .== i)
-    end
-    return instances
+   instances = zeros(size(y, 1))
+   sparse_y = onecold(y)
+   for i in 1:length(instances)
+       instances[i] = sum(sparse_y .== i)
+   end
+   return instances
 end
+
 
 # Precision
 function Precision(y_pred, y_true; avg_type="macro", sample_weights=nothing)
-    TP, TN, FP, FN = TPFN(y_pred, y_true)
+    _,TP, TN, FP, FN = TFPN(y_pred, y_true)
     # Macro-averaged Precision
     if avg_type == "macro"
-        return mean(TP ./ (TP .+ FP))
+        return mean(TP ./ (TP .+ FP .+ eps(eltype(TP))))
     # Micro-averaged Precision
-    else if avg_type == "micro"   
+    elseif avg_type == "micro"   
         return mean(TP) / (mean(TP) + mean(FP))
     # Weighted-Averaged Precision
     else
@@ -125,18 +125,18 @@ function Precision(y_pred, y_true; avg_type="macro", sample_weights=nothing)
         else
             weights = calc_instances(y_true) / size(y_true, 2)
         end
-        return mean((TP ./ (TP .+ FP)) .* weights)
+        return mean((TP ./ (TP .+ FP .+ eps(eltype(TP)))) .* weights)
     end
 end
 
 # Recall
 function Recall(y_pred, y_true; avg_type="macro", sample_weights=nothing)
-    TP, TN, FP, FN = TPFN(y_pred, y_true)
+    _,TP, TN, FP, FN = TFPN(y_pred, y_true)
     # Macro-averaged Precision
     if avg_type == "macro"
-        return mean(TP ./ (TP .+ FN))    
+        return mean(TP ./ (TP .+ FN .+ eps(eltype(TP))))    
     # Micro-averaged Precision
-    else if avg_type == "micro"   
+    elseif avg_type == "micro"   
         return mean(TP) / (mean(TP) + mean(FN))
     # Weighted-Averaged Precision
     else
@@ -146,15 +146,15 @@ function Recall(y_pred, y_true; avg_type="macro", sample_weights=nothing)
         else
             weights = calc_instances(y_true) / size(y_true, 2)
         end
-        return mean((TP ./ (TP .+ FN)) .* weights)
+        return mean((TP ./ (TP .+ FN .+ eps(eltype(TP)))) .* weights)
     end
 end
 
 # F_beta score
 function F_beta_score(y_pred, y_true; β=1, avg_type="macro", sample_weights=nothing)
-    recall_ = Recall(y_pred, y_true, avg_type, sample_weights)
-    precision_ = Precision(y_pred, y_true, avg_type, sample_weights)
-    return (1 + β^2) * precision_ * recall_ / (precision_ + (β^2) * recall_)
+    recall_ = Recall(y_pred, y_true, avg_type=avg_type, sample_weights=sample_weights)
+    precision_ = Precision(y_pred, y_true, avg_type=avg_type, sample_weights=sample_weights)
+    return (1 + β^2) * precision_ * recall_ / (precision_ + (β^2) * recall_ + eps(eltype(recall)))
 end
 
 # Sensitivity
@@ -163,13 +163,13 @@ const Sensitivity = Recall
 const Detection_rate = Recall
 
 # Specificity
-function Specificity()
-    TP, TN, FP, FN = TPFN(y_pred, y_true)
+function Specificity(y_pred, y_true; avg_type="macro", sample_weights=nothing)
+    _, TP, TN, FP, FN = TFPN(y_pred, y_true)
     # Macro-averaged Precision
     if avg_type == "macro"
-        return mean(TN ./ (TN .+ FP))    
+        return mean(TN ./ (TN .+ FP .+ eps(eltype(TP))))    
     # Micro-averaged Precision
-    else if avg_type == "micro"   
+    elseif avg_type == "micro"   
         return mean(TN) / (mean(TN) + mean(FP))
     # Weighted-Averaged Precision
     else
@@ -179,7 +179,7 @@ function Specificity()
         else
             weights = calc_instances(y_true) / size(y_true, 2)
         end
-        return mean((TN ./ (TN .+ FP)) .* weights)
+        return mean((TN ./ (TN .+ FP .+ eps(eltype(TP)))) .* weights)
     end
 end
 
@@ -190,7 +190,7 @@ end
 
 # Cohen's Kappa
 function Cohen_Kappa(y_pred, y_true)
-    TP, TN, FP, FN = TFPN(y_pred, y_true)
+    _, tp, tn, fp, fn = TFPN(y_pred, y_true)
     mrg_a = ((tp .+ fn) .* (tp .+ fp)) ./ (tp .+ fn .+ fp .+ tn)
     mrg_b = ((fp .+ tn) .* (fn .+ tn)) ./ (tp .+ fn .+ fp .+ tn)
     expec_agree = (mrg_a .+ mrg_b) ./ (tp .+ fn .+ fp .+ tn)
@@ -202,23 +202,23 @@ end
 # StatsfromTFPN
 function StatsfromTFPN(TP, TN, FP, FN)
     Confusion_Matrix = reshape([TP, FP, FN, TN], 2, 2)
-    Precision = TP / (TP + FP)
-    Recall = TP / (TP + FN)
-    Specificity = TN / (TN + FP)
-    F1_score = 2 * (Precision * Recall) / (Precision + Recall)
+    Precision = TP / (TP + FP + eps(eltype(TP)))
+    Recall = TP / (TP + FN + eps(eltype(TP)))
+    Specificity = TN / (TN + FP + eps(eltype(TP)))
+    F1_score = 2 * (Precision * Recall) / (Precision + Recall + eps(eltype(TP)))
     Accuracy = (TP + TN) / (TP + TN + FP + FN)
-    False_alarm_rate = 1 - specificity
-    return Dict("Confusion_Matrix" => Confusion_Matrix,
-                "Precision" => Precision, "Recall" => Recall,
-                "Specificity" => Specificity, "F1_score" => F1_score,
-                "Accuracy" => Accuracy)
+    False_alarm_rate = 1 - Specificity
+    return Dict(:Confusion_Matrix => Confusion_Matrix,
+                :Precision => Precision, :Recall => Recall,
+                :Specificity => Specificity, :F1_score => F1_score,
+                :Accuracy => Accuracy)
 end
 
 # Classwise Stats
 function Classwise_Stats(y_pred, y_true)
-    TP, TN, FP, FN = TFPN(y_pred, y_true)
+    _, TP, TN, FP, FN = TFPN(y_pred, y_true)
     ClasswiseStats = Dict() 
-    for i in 1:size(y_true, 2)
+    for i in 1:size(y_true, 1)
         ClasswiseStats[i] = StatsfromTFPN(TP[i], TN[i], FP[i], FN[i])
     end
     return ClasswiseStats
@@ -227,28 +227,26 @@ end
 # Global Stats
 # TODO: add weighted stats option as above functions
 function Global_Stats(y_pred, y_true; avg_type="macro")
-    TP, TN, FP, FN = TFPN(y_pred, y_true) 
+    confusion_matrix_, TP, TN, FP, FN = TFPN(y_pred, y_true) 
     if avg_type == "macro"
-        confusion_matrix = Confusion_matrix(y_pred, y_true)
-        precision = mean(TP ./ (TP .+ FP))
-        recall = mean(TP ./ (TP .+ FN))
-        f1_score = 2 * precision * recall / (precision + recall)
-        specificity = mean(TN ./ (TN .+ FP))
-        accuracy = Categorical_accuracy(y_pred, y_true)
+        precision = mean(TP ./ (TP .+ FP .+ eps(eltype(TP))))
+        recall = mean(TP ./ (TP .+ FN .+ eps(eltype(TP))))
+        f1_score = 2 * precision * recall / (precision + recall + eps(eltype(TP)))
+        specificity = mean(TN ./ (TN .+ FP .+ eps(eltype(TP))))
+        accuracy = categorical_accuracy(y_pred, y_true)
         false_alarm_rate = 1 - specificity
-        return Dict("Confusion_Matrix" => confusion_Matrix,
+        return Dict("Confusion_Matrix" => confusion_matrix_,
                 "Precision" => precision, "Recall" => recall,
                 "Specificity" => specificity, "F1_score" => f1_score,
                 "Accuracy" => accuracy, "False_alarm_rate" => false_alarm_rate)
-    else if avg_type == "micro"
-        confusion_matrix = Confusion_matrix(y_pred, y_true)
+    elseif avg_type == "micro"
         precision = mean(TP) / (mean(TP) + mean(FP))
         recall = mean(TP) / (mean(TP) + mean(FN))
-        f1_score = 2 * precision * recall / (precision + recall)
+        f1_score = 2 * precision * recall / (precision + recall + eps(eltype(TP)))
         specificity = mean(TN)/ (mean(TN) + mean(FP))
-        accuracy = Categorical_accuracy(y_pred, y_true)
+        accuracy = categorical_accuracy(y_pred, y_true)
         false_alarm_rate = 1 - specificity
-        return Dict("Confusion_Matrix" => confusion_Matrix,
+        return Dict("Confusion_Matrix" => confusion_matrix_,
                 "Precision" => precision, "Recall" => recall,
                 "Specificity" => specificity, "F1_score" => f1_score,
                 "Accuracy" => accuracy, "False_alarm_rate" => false_alarm_rate)
--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,33 @@
 # Util functions
-include("Classification.jl")
+using DataFrames
+include("./Classification.jl")
 
-function display_stats(y_pred, y_true, avg_type,)
+function report_stats(y_pred, y_true; classwise_stats=true, avg_type="macro", sample_weights=nothing)
+    g_stats = Global_Stats(y_pred, y_true, avg_type = avg_type)
+    print("++++++++++++++++++++ Confusion Matrix +++++++++++++++++++\n")
+    cm = g_stats["Confusion_Matrix"]
+    for i in 1:size(cm,1)
+        print(" ")
+        for j in 1:size(cm,2)
+            print(cm[i,j]," ")
+        end
+        print("\n")
+    end
+    print("\n")
+    print("++++++++++++++++++++ Global Stats ++++++++++++++++++++\n")
+    print("  Accuracy: ", g_stats["Accuracy"],"\n")
+    print("  Precision: ", g_stats["Precision"],"\n")
+    print("  Recall: ", g_stats["Recall"],"\n")
+    print("  F1_score: ", g_stats["F1_score"],"\n")
+    print("  Specificity: ", g_stats["Specificity"],"\n")
+    print("  False alarm rate: ", g_stats["False_alarm_rate"],"\n")
+    print("\n")
+    if (classwise_stats == true)
+        c_stats = Classwise_Stats(y_pred, y_true)
+        stats = DataFrame(Class=[], Confusion_Matrix=[], Accuracy=[], Precision=[], Recall=[], F1_score=[], Specificity=[])
+        for i in 1: length(c_stats)
+            push!(stats, Dict(:Class=>i,c_stats[i]...))
+        end
+        print(stats)
+    end
+end
